da93f483eb1a2075322ac2072a365b5c7056557c
AlexanderHBauer <alexander.heinr.bauer@gmail.com>
2021-04-11 13:17:18 +0200

    Add files via upload

---
+++ RegexEdu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RegexEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexService rx = new();
+
+            // string for testing
+            string authors = "Mahesh Chand, Raj Kumar, Mike Gold, Allen O'Neill, Marshal Troll, mini michael";
+
+            // Match a basic character or char set
+            //rx.BasicCharSetMatch_CaseSensitive(authors);
+            //rx.BasicCharSetMatch_CaseInSensitive(authors);
+            //rx.Split();
+            rx.TestProgram();
+            Console.ReadKey();
+        }
+    }
+}
